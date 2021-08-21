e4e97d72eb66af4822501ebb15d5450d7dc40089
Melanie Safira <melaniesafira94@gmail.com>
2021-08-21 11:56:04 +0700

    Initial commit

---
+++ lib/shared/shared_value.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+String apiKey = '9ad38a080de1360ef8a9a9e26433fbbc';
+String imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+PageEvent prevPageEvent;
+File imageFileToUpload;
